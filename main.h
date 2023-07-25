56488b139e1c7a96ed30f36e09a2b3e9dc1727d2
Frity2025 <frity362@gmail.com>
2023-07-24 23:43:24 -0700

    main.h updated

--- main.h
+++ main.h
@@ -1,5 +1,5 @@
-#ifndef SHELL_H
-#define SHELL_H
+#ifndef MAIN_H
+#define MAIN_H
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -11,11 +11,21 @@
 #include <errno.h>
 
 extern char **environ;
-
+int sh_exit(char **args);
+char *builtin_str[] = {"exit"};
+int (*builtin_func[])(char **) = {&sh_exit};
+int sh_exit(char **args);
+int _fork_fun(char **arg, char **av, char **env, char *lineptr, int np, int c);
+void _getenv(char **env);
+char *_strcat(char *dest, const char *src);
+int _strcmp(char *s1, char *s2);
+int _strlen(char *str);
+size_t _strncmp(char *s1, char *s2, size_t n);
+char *_strcpy(char *dest, char *src);
 int sh_exit(char **args);
 int sh_num_builtins(void)
 char *_get_path(char **env)
 int _fork_fun(char **arg, char **av, char **env, char *lineptr, int np, int c);
 
-#endif /* SHELL_H */
+#endif /* MAIN_H */
 
