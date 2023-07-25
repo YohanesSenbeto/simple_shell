f5c7f8754258afeecd153922d1ad6adc805cfabb
Frity2025 <frity362@gmail.com>
2023-07-24 23:05:25 -0700

    tokenization

---
+++ tokenization.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * tokenize - tokenizes a string
+ * @lineptr: what the user typed
+ * Return: a ptr to arr of pointers
+ */
+
+char **tokenize(char *lineptr)
+{
+    char **new_array = NULL;
+    char *token_generator = NULL;
+    size_t i = 0;
+    int new_length = 0;
+
+    if (lineptr == NULL)
+        return (NULL);
+
+    for (i = 0; lineptr[i]; i++)
+    {
+        if (lineptr[i] == ' ')
+            new_length++;
+    }
+    if ((new_length + 1) == _strlen(lineptr))
+        return (NULL);
+    new_array = malloc(sizeof(char *) * (new_length + 2));
+    if (new_array == NULL)
+        return (NULL);
+
+    token_generator = strtok(lineptr, " \n\t\r");
+
+    for (i = 0; token_generator != NULL; i++)
+    {
+        new_array[i] = token_generator;
+        token_generator = strtok(NULL, " \n\t\r");
+    }
+    new_array[i] = NULL;
+    return (new_array);
+}
